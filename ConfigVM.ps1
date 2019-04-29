550e82fba560be471fb0383e9c6944ea8e2f011d
WeiFeng <alanniu86@gmail.com>
2019-04-29 17:20:05 +0800

    Update ConfigVM.ps1

--- ConfigVM.ps1
+++ ConfigVM.ps1
@@ -172,7 +172,8 @@ General notes
 #>
 
 function installChocolatey {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    # Set-ExecutionPolicy Bypass -Scope Process -Force; 
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 function installTools {
     choco install webdeploy
